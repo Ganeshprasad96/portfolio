4f5a982e530f1582bf8b6bfbbe469ae3926990bb
pratyushcse <pratyushsuvarna4@gmail.com>
2024-10-03 12:06:47 +0530

    first commit

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -3,19 +3,19 @@ const DUMMY_PROJECTS=[
         projectTitle:'PortFolio Template',
         description: 'This project is a React app to generate a creative professional website based on Data provided. The website uses no backend and is free to fork and use.',
         sourceLink:'https://github.com/killer-abhi/Portfolio.git',
-        lastUpdated:'Jan 20 2023'
+        lastUpdated:'Sep 20 2024'
     },
     {
         projectTitle:'Temple Website',
         description: "Temple website using HTML and CSS that provides information about daily prayers, rituals, and festivals. It features a clean layout to showcase temple traditions and events",
         sourceLink:'https://ganeshprasad18.github.io/ganesh2/project',
-        lastUpdated:'Dec 30 2024'
+        lastUpdated:'Sep 30 2024'
     },
     {
         projectTitle:'Calculator',
         description: 'It is a Realtime Chat Website built using Socket.io and Express with NodeJs as a backend and MongoDB as a database. It updates the chat messages in realtime with Typing notification of other users',
         sourceLink:'https://github.com/killer-abhi/realtime-chat-site.git',
-        lastUpdated:'Dec 10 2022'
+        lastUpdated:'Sep 20 2024'
     },
     
 ];
